7b8b1ee98c11f5ebb29ea87d365e8b1f1afbec5d
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-05-28 18:09:42 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        list1 = [0] * 26
+        list2 = [0] * 26
+
+        for ch in s:
+            list1[ord(ch) - ord('a')] += 1
+        
+        for ch in t:
+            list2[ord(ch) - ord('a')] += 1
+        
+        if list1 == list2:
+            return True
+        else:
+            return False
+        
