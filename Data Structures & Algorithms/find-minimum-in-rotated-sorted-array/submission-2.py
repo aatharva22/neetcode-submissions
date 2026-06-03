8275583306b54ec3981164cb8ff093c99ec9b658
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-06-03 12:02:51 -0400

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+
+            mid = (left + right) // 2
+            #The rotation is in clockwise direction so when mid > left and mid>right
+            # the drop will be in mid, right
+            if nums[mid] > nums[right]:
+                #drop happened here, so ans in mid+1, right region
+                left = mid + 1
+            else:
+                #drop did not happen in mid, right. So it must have happened in 
+                # left to mid
+                right = mid
+        #returning left or right is the same, as both are equal, loop breaks there
+        return nums[left]   
+
