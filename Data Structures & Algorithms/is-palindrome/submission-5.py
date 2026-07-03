16deef0ec65444a10d64185fde051c4e23238ebc
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-07-03 16:33:13 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        
+        onlyAlnums = "".join(ch.lower() for ch in s if ch.isalnum())
+        if len(onlyAlnums) == 0:
+            return True
+        left = 0
+        right = len(onlyAlnums) - 1
+
+        while left < right:
+
+            if onlyAlnums[left] != onlyAlnums[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True 
