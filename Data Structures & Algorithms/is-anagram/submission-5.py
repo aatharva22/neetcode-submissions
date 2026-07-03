8ffcb2128c5d585e7652514df5bc417d7ff1edb8
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-07-03 13:25:16 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        arr1 = [0] * 26
+        arr2 = [0] * 26
+
+        for i in range(len(s)):
+            arr1[(ord(s[i]) - ord('a'))] += 1
+            arr2[(ord(t[i]) - ord('a'))] += 1
+        
+        for i in range(26):
+            if arr1[i] != arr2[i]:
+                return False
+        return True
+        
