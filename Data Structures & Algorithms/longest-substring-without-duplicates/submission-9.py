8110a1c761438a8e8cd3dbaba4504680e0deeb01
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-07-03 17:49:23 -0400

    Add: longest-substring-without-duplicates - submission-9

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-9.py
@@ -0,0 +1,27 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        hmap = {}
+
+        left = 0
+        right = 0
+        longest = 0
+
+        while right < len(s):
+
+            if s[right] in hmap:
+
+                while s[left] != s[right] and left <= right:
+                    del(hmap[s[left]])
+                    left += 1
+                del(hmap[s[left]])
+                left += 1
+
+
+            else:
+                hmap[s[right]] = 1
+                longest = max(longest, right - left + 1)
+                right += 1
+        
+        return longest
+            
