43e7930e0a4bc7656c58a1706fdb95b512646ae7
Atharva Parag Sonawane <atharvapu10@gmail.com>
2026-07-17 10:30:01 -0400

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        # Either we can reach n steps by taking 1 step from n-1
+        # or 2 steps from n-2
+        #so unique ways(n) = unique ways(n-1) + unique ways(n-2)
+
+        if n <= 2:
+            return n
+        prev, curr = 1 , 2
+
+        for _ in range(3,n+1):
+
+            prev,curr = curr, curr+prev
+        return curr
+
+
+
